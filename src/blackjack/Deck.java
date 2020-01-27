c07084db3f7489022ac49b4f4d1d211a48fdd7ef
likhithborela <borela001@gannon.edu>
2020-01-27 17:23:56 -0500

    complete deck class structure

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,5 +10,29 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
-    
+    //initializing variables 
+    private Card[] myCards = new Card[52];
+    private String[] ranks, suits = {};
+    private int nextCard = 0;
+    //the constructor
+    public Deck(){
+
+    }
+    //initializes a deck by creating deck objects and assigning them into the myCards array
+    private void initDeck(){
+
+    }
+    //shuffles the myCards arrays values through a math.random loop
+    private void shuffle(){
+
+    }
+    //test method
+    public void printDeck(){
+
+    }
+    //deals card to a player from the array
+    public Card dealCard(){
+        return null;
+    }
+
 }

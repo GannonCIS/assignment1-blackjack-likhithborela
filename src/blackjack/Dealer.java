5dffae5b4fc0f3abba6c2eb07c243b0c6fb34e8e
likhithborela <borela001@gannon.edu>
2020-02-02 13:42:33 -0500

    complete revision 1 of class structure of dealer class

--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -7,8 +7,40 @@ package blackjack;
 
 /**
  *
- * @author gubotdev
+ * @author borela001
  */
 public class Dealer {
+
+    private Hand dealerHand = new Hand();
+    private Deck gameDeck = new Deck();
+
+    private boolean isDealerBust;
+
+    public Dealer(Hand dealerHand, Deck gameDeck){
+        this.dealerHand = dealerHand;
+        this.gameDeck = gameDeck;
+    }
+
+    private Card dealOpeningHand(){
+        //i dont really know what data type this returns, if any, i am thinking cards but i dont know
+        return null;
+    }
+
+    private Card takePlayerTurns(){
+        return null;
+    }
+
+    private boolean playOutDealerHand(){
+        return isDealerBust;
+    }
+
+    private String declareWinner(){
+        return null;
+    }
+
+    public void playGame(){
+
+    }
+    
     
 }

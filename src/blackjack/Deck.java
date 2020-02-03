3094e358cd175b22e881f6028909cf5a0138bf97
likhithborela <borela001@gannon.edu>
2020-02-03 15:50:09 -0500

    add deck contructor, printDeck logic, initDeck logic, and initialize suits and ranks array

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -5,6 +5,7 @@
  */
 package blackjack;
 
+
 /**
  *
  * @author gubotdev
@@ -12,23 +13,31 @@ package blackjack;
 public class Deck {
     //initializing variables 
     private Card[] myCards = new Card[52];
-    private String[] ranks, suits = {};
+    private String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"}; 
+    private String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
     private int nextCard = 0;
     //the constructor
     public Deck(){
-
+        initDeck();
     }
     //initializes a deck by creating deck objects and assigning them into the myCards array
     private void initDeck(){
-
+        for(int s = 0; s < suits.length; s++){
+            for(int r = 0; r < ranks.length; r++){
+                myCards[(s*13) + r] = new Card(ranks[r], suits[s]);
+            }
+        }
     }
     //shuffles the myCards arrays values through a math.random loop
     private void shuffle(){
 
     }
+    
     //test method
     public void printDeck(){
-
+        for(int i = 0; i < myCards.length;i++){
+            System.out.println(myCards[i].RANK + " of " + myCards[i].SUIT);
+        }
     }
     //deals card to a player from the array
     public Card dealCard(){
